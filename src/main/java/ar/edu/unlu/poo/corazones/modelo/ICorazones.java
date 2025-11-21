f6d9cb7801333056c1c2b160dfa08f6c803840e0
EliseoSanchez <eliseosanchez99@yahoo.com>
2025-11-21 16:07:33 -0300

    .

--- src/main/java/ar/edu/unlu/poo/corazones/modelo/ICorazones.java
+++ src/main/java/ar/edu/unlu/poo/corazones/modelo/ICorazones.java
@@ -6,6 +6,25 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface ICorazones extends IObservableRemoto {
+
+    // Agregar un jugador a la partida
     void agregarJugador(Jugador jugador) throws RemoteException;
+
+    // Obtiene la lista de jugadores
     List<Jugador> getJugadores() throws RemoteException;
+
+    //inicia la partida
+    void iniciarPartida() throws RemoteException;
+
+    // Jugar una ronda pasando las cartas en orden
+    Jugador jugarRonda(List<Carta> cartasEnOrden) throws RemoteException;
+
+    // Avanzar a la siguiente ronda y actualizar puntajes
+    void siguienteRonda() throws RemoteException;
+
+    // Obtener el ganador de la partida (el que tenga menos puntos)
+    Jugador obtenerGanador() throws RemoteException;
+
+    // Aplicar el intercambio de cartas según la ronda
+    void aplicarIntercambio(List<List<Carta>> cartasIntercambio) throws RemoteException;
 }
